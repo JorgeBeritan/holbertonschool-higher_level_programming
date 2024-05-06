f3853f984f280be83d7b13994dbf9f3a59c73033
JorgeBeritan <inaikmcontacto@gmail.com>
2024-05-06 11:08:46 -0300

    Comb2

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@ for i in range(0, 9):
     for i2 in range(i + 1, 10):
         if i == 8:
             print("{:d}{:d}".format(i, i2))
+            break
         print("{:d}{:d}".format(i, i2), end=", ")
