2cb58f694e00343339b60759ae35afaa7d68af1b
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-13 11:52:41 +0000

    Fix pydecodestyle problem

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1:
         return my_list
     else:
-         new_list[idx] = element
-         return new_list
+        new_list[idx] = element
+        return new_list
