aa9c9f421fb911714489b6d960b2627ece1103e6
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-14 20:09:40 +0000

    First commiy

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
+    traduced = 0
+    for char in roman_string:
+        traduced += roman[char]
+        ""
+    return traduced
