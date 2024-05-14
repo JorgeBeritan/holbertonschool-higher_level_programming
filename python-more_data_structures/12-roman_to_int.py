815d20619f5dbc2a059c208ab732c96f9d20ce3d
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-14 20:49:46 +0000

    Now is funcionaly

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
     roman = {
         "I" : 1,
         "V" : 5,
@@ -10,7 +12,15 @@ def roman_to_int(roman_string):
         "M" : 1000
     }
     traduced = 0
+    char_prev = 0
     for char in roman_string:
-        traduced += roman[char]
-        ""
+        if char in roman:
+            traduced += roman[char]
+            if roman[char] > char_prev:
+                traduced -= char_prev * 2
+            char_prev = roman[char]
+        else:
+            return 0
+        
     return traduced
+
