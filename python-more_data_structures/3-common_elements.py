a15e4518d6ede1ee69790ef6a0aad8d459aa56c1
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-14 12:38:24 +0000

    Fix pycodestyle isuue

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,9 +3,9 @@ def common_elements(set_1, set_2):
     set1 = list(set_1)
     set2 = list(set_2)
     value = []
-    
+
     for i in set1:
         for i2 in set2:
             if i == i2:
                 value += [i2]
-    return value 
+    return value
