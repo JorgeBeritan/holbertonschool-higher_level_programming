78a108e8208433e56d82858fe9aa530b895570a6
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-17 11:24:20 +0000

    Loop changes for check the condition

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for _ in my_list:
-        count += 1
-    if count < x:
-        return
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-        return x
-    except IndexError:
-        return x
-
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
