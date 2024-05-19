a7477a9b4626bbaff5736fa0d6c795e9190198f9
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-19 18:53:15 +0000

    comment

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Empty class 'Square'
-"""
+"""This module defines an empty class named 'Square'."""
+# Define an empty class named Square
 class Square:
-    """Definition 'Square'
-    """
+    """This is the definition of the 'Square' class, which is currently empty."""
+    # Currently, this class does not have any attributes or methods
