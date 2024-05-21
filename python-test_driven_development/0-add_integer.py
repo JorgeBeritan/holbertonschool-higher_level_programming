cacaee27a224ea927387d195dd351da972a88d33
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-21 17:14:20 +0000

    Docstring fix line

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,13 +3,7 @@
 """
 def add_integer(a, b=98):
     """Add two integers.
-    Args:
-        a (int or float): The first number.
-        b (int or float, optional): The second number. Defaults to 98.
-    Returns:
-        int: The sum of the two numbers.
-    Raises:
-        TypeError: If either `a` or `b` is not an integer or float.
+    Return a and b
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
