1f6550687d02fdcf63f2f9d50ad83ad135a3cde5
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-21 16:35:52 +0000

    Pycodestyle fix error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """Is a function to add a and b
-
 """
 def add_integer(a, b=98):
     """Add two integers.
-
     Args:
         a (int or float): The first number.
         b (int or float, optional): The second number. Defaults to 98.
-
     Returns:
         int: The sum of the two numbers.
-
     Raises:
         TypeError: If either `a` or `b` is not an integer or float.
-
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
@@ -23,7 +18,8 @@ def add_integer(a, b=98):
     # Convert to integer if a is float
     if type(a) is float:
         a = int(a)
-     # Convert to integer if b is float
+    # Convert to integer if b is float
     if type(b) is float:
         b = int(b)
+
     return a + b
