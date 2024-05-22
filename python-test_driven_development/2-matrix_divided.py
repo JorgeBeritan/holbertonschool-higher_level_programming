b7c80cc51ef62e6b0caca63ab86baf3695f31ef4
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-22 11:19:38 +0000

    Pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,15 +6,15 @@
 def matrix_divided(matrix, div):
     """Divide a matrix with this function
     """
+    matrixerror = "matrix must be a matrix (list of lists) of integers/floats"
     if not matrix or not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) \
-                        of integers/floats")
+        raise TypeError(matrixerror)
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(matrixerror)
         for item in row:
             if not isinstance(item, (float, int)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(matrixerror)
     for i in range(len(matrix) - 1):
         if len(matrix[i]) != len(matrix[i + 1]):
             raise TypeError("Each row of the matrix must have the same size")
