d1f0d9f0fae05271beb0adb3f7baa5940cecb5a4
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-22 12:29:48 +0000

    First commit

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function for indentation a text
+"""
+
+
+def text_indentation(text):
+    """Implement a few border case
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    text = text.replace(".", ".\n\n").replace(":", ":\n\n").replace("?", "?\n\n")
+    text = text.replace("\n ", "\n")
+    print(text)
