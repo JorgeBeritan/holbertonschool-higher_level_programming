7cb93ffe96f1719ec4672a7a65a4838ef085c8df
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-23 11:42:21 +0000

    Prove another test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,26 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         lista = [1, 2, 3, 4, 5]
         self.assertEqual(max_integer(lista), max(lista))
-
+    
+    def test_max_integer_beginning(self):
+        lista = [7, 1, 3, 4, 5, 6]
+        self.assertEqual(max_integer(lista), max(lista))
+    
+    def test_max_integer_middle(self):
+        lista = [1, 2, 3, 7, 5, 6]
+        self.assertEqual(max_integer(lista), max(lista))
+    def test_max_integer_negative(self):
+        lista = [1, 2, 3, -50, 68, -2]
+        self.assertEqual(max_integer(lista), max(lista))
+    def test_max_integer_negative_only(self):
+        lista = [-1, -2, -3, -100, -2]
+        self.assertEqual(max_integer(lista), max(lista))
+    def test_max_intger_one_argument(self):
+        lista = [1]
+        self.assertEqual(max_integer(lista), max(lista))
+    def test_max_integer_empty(self):
+        lista = []
+        self.assertEqual(max_integer(lista), max(lista))
 
 if __name__ == "__main__":
     unittest.main()
