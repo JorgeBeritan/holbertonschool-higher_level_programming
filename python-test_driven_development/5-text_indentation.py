cec3de021194bace1d91933139debe9468265ecd
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-23 11:09:11 +0000

    Fix bug in :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,7 +8,6 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-
-    text = text.replace(".", ".\n\n").replace(":", ":\n\n").replace("?", "?\n\n")
-    text = text.replace("\n ", "\n")
-    print(text, end="")
+    text = text.replace(".", ".\n\n").replace(":", ":\n\n")
+    text = text.replace("?", "?\n\n").replace("\n ", "\n")
+    print(text)
