994efdcc55456b828f42099b80cce7c53bf0f65d
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-24 13:52:00 +0000

    First commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""In this function we get what kind of class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """In this function we use isintance to get a kind a class is
+    """
+
+    return isinstance(obj, a_class)
