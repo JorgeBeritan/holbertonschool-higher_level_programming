eb3b08d36fa6d5931ff22bb0e878d964accb2925
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-24 17:55:17 +0000

    Fix class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""Area class add
+"""Inherit geometry
 """
 
-class BaseGeometry:
-    """A base Geometry
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """In this class inherits of BaseGeometry
     """
     def __init__(self, width, height):
-        """Constructo init
-        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-    def area(self):
-        raise Exception("area() is not implemented")
