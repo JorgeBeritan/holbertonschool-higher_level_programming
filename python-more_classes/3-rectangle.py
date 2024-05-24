e3d69ded664d6792521b5719d7dada60ce889a64
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-24 11:20:38 +0000

    First commit

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,6 +45,6 @@ class Rectangle:
 
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         row = ["#" * self.__width for _ in range(self.__height)]
         return "\n".join(row)
