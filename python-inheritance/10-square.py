c4760b451d45b0d129cefa00bacd1d66e93ba739
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-26 00:13:37 +0000

    First commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Inherit Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+
+class Square(Rectangle):
+    """Inherits of inherits
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
