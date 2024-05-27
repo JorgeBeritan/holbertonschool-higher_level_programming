565dd9e0aa1f6da503d843b78dcb3f591f695361
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-27 09:47:28 +0000

    Module comment

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""In this fucntion we learn how we open a file
+"""
+
 
 def read_file(filename=""):
+    """In this function we open a file and print the content
+    """
     with open(filename, "r", encoding="ustf-8") as line:
         mostrar = line.read()
     print(mostrar)
