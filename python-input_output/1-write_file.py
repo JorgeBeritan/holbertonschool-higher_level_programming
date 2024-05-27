1adb566899d41259d2bbf061c1f5a7590e99f05a
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-27 09:59:10 +0000

    First commit

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""In this function we learn about write in a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """In this function we implement a with reserveted word write in a file
+    """
+    with open(filename, "w", encoding="uft-8") as archive:
+        archive.write(text)
+    archive.close()
+    return len(text)
