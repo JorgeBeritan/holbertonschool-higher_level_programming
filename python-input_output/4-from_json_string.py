bfc6547ecefc89d84f456e282180c70603520e96
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-28 12:29:04 +0000

    First commit

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""By me
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """In this function we learn how represent a object in python representation json
+    """
+    var = json.loads(my_str)
+    return var
