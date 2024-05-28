a2ae3c4f3c094ce29aa023462558daa7e539e412
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-28 13:50:19 +0000

    First commit

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/ptyhon3
+"""By me
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """In this function we represent a json write in a archive
+    """
+    with open(filename, "w") as archive:
+        archive.write(json.dumps(my_obj))
