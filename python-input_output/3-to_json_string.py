ab31a28da6d53e8817f3b7c8e081f1d62be5e5b9
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-28 12:24:11 +0000

    First commit

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""By me
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """In this function we learn how represent a json
+    """
+    var = json.dumps(my_obj)
+    return var
