7327fe67af482df4d868d562d852cda1156f200d
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-28 12:29:52 +0000

    Fix bug in PEP8

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,7 +7,8 @@ import json
 
 
 def from_json_string(my_str):
-    """In this function we learn how represent a object in python representation json
+    """In this function we learn how represent a object
+    in python representation json
     """
     var = json.loads(my_str)
     return var
