7b1ccf11d84c772d8a69c359c04fff38aafe713d
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-28 14:26:12 +0000

    First commit

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""By me
+"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """In this function we can learn how create an objecto from a json file
+    """
+    with open(filename, "r", encoding="utf-8") as archive:
+        extract = archive.read()
+        load = json.loads(extract)
+    return load
