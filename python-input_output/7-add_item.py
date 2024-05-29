3589e9d62c1526ed11c5fe015dc21d9ac5436218
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-29 13:35:48 +0000

    First commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-import sys
+"""By me
+"""
 
-with open("prueba.txt", "w", encoding="utf-8") as archive:
-    for i in sys.argv[1:]:
-        archive.write(str(sys.argv))
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    argument = load_from_json_file('add_item.json')
+except:
+    argument = []
+
+for i in range(1, len(sys.argv)):
+    argument.append(sys.argv[i])
+
+save_to_json_file(argument, 'add_item.json')
