6ceaf171b756075f37b090c55589544e277efdc8
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-05-31 16:12:05 +0000

    First commit

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,21 +2,20 @@
 """Made by Jorge Beritan
 """
 
-
 import csv
 import json
 
 
 def convert_csv_to_json(filename):
-    """Convert csv to json
+    """In this function we learn how cconvert a csv to json
     """
     try:
         with open(filename, "r") as archive:
             csv_reader = csv.DictReader(archive)
             data = [row for row in csv_reader]
-    
-        with open(filename, "w") as archive_json:
-            json.dumps(data, archive_json)
+
+        with open("data.json", "w") as archive:
+            archive.write(json.dumps(data))
         return True
     except FileNotFoundError:
         return False
