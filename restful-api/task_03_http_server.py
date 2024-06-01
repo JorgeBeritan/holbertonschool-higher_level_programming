8e9884107d4f1f4bfd81825c62b564a682114aa1
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-06-01 14:58:38 +0000

    First comiit

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from http.server import *
+import json
+
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-type", "aplication/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        else:
+            self.send_error(404, "Not found")
+
+
+port = HTTPServer(("", 8000), Server)
+port.serve_forever()
+port.server_close()
