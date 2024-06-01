3b79a0ec089eeed393ababb91251c6eeb13f61d5
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-06-01 16:58:01 +0000

     

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -28,11 +28,6 @@ class Server(BaseHTTPRequestHandler):
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode())
-        elif self.path == '/undefined':
-            self.send_response(404)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.send_error(404, "Endpoint not found")
         else:
             self.send_response(404)
             self.send_error(404, "Endpoint not found")
