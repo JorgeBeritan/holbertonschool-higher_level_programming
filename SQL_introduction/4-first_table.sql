5301905ed0a433f3152f18dbec710e4d9b61f0cb
JorgeBeritan <inaikmcontact@gmail.com>
2024-06-20 12:11:58 -0300

    NOW

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- Create a first table 
-USE;
-CREATE TABLE first_table(id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256) NOT NULL);
