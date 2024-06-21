062e723323dfa0b846f4b9a8925da47a93601777
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-21 14:30:30 -0400

    Fix bug with second user

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Show a privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost' SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
