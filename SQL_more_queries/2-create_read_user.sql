58f3f48dfeaae73dbfa4360073512b7791ce82a2
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-24 06:04:23 -0400

    FIX databases create

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create a database and user
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0c_0.* TO 'user_0d_2'@'localhost';
