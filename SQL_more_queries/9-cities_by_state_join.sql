ca09797baa75a82581da078e5fa7d8a4d4178405
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-24 10:46:24 -0400

    INNER JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- list all cities from all states
-SELECT cities.id, cities.name, state.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
