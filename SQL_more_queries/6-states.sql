f876155bd8dfc883b1bdbd1c7a188ce535dd66f3
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-24 06:31:52 -0400

    Add use database

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- Create a databases and table with unqui ID primery key and auto generated
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
