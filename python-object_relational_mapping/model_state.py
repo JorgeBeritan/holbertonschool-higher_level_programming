25072c78b24163e77d80a88c292e922c2253bd55
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-29 10:31:32 -0400

    comment add

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
 
+"""
+In this script we learn how inserte a table with sqlalchemy
+"""
+
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 
 
 class State(Base):
+    """
+    In this clase we create state
+    """
     __tablename__ = "states"
 
     id = Column(Integer(), primary_key=True, nullable=False, autoincrement=True)
