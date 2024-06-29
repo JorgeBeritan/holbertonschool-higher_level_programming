b86cd333bd406b9bac89fd682047708aeb379f8a
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-29 08:26:25 -0400

    Fix document

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+def cities_state(username, password, database):
+    try:
+        db = MySQLdb.connect(
+            host="localhost";
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+        cursor = db.cursor
+        query = "SELECT cities.id, cities.name, states.name FROM \
+                cities INNER JOIN states ON cities.state_id = state.id\
+                ORDER BY cities.id ASC"
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for i in result:
+            print(i)
+
+    except MySQLdb as e:
+        print(f"error:{e}")
+    finally:
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    cities_state(username, password, database);
