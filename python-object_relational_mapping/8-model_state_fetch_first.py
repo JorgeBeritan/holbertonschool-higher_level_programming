7b5a41a1aea348f9d775efdd1eb4f97d3530fa95
JorgeBeritan <inaikmcontacto@gmail.com>
2024-06-30 11:12:55 -0400

    First commit

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+def list_id(username, password, database):
+    dburl = ('mysql+mysqldb://{}:{}@localhost/{}'
+          .format(username, password, database))
+
+    engine = create_engine(dburl)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.id == 1)
+
+    for i in query:
+        print("{}: {}".format(i.id, i.name))
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_id(username, password, database)
