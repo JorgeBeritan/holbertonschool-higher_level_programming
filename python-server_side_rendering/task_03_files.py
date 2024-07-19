fdbdaacb67e071f5d552f8003d630495ce88cf40
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-19 14:21:05 -0300

    first commit

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open("items.json", "r") as file:
+        data = json.load(file)
+
+    return render_template('items.html', data=data.get('items', []))
+
+@app.route('/products')
+def products():
+    source = request.args.get('source', '')
+    product_id = request.args.get('id')
+
+    if source not in ['json', 'csv']:
+        return render_template('product_display.html', error="Wrong source")
+
+    file = f"products.{source}"
+    def rjf(file):
+        with open(file, "r") as file:
+            return json.load(file)
+
+    def rcf(file):
+        with open(file, 'r') as file:
+            csv_reader = csv.DictReader(file)
+            return [
+                    {key: float(value) if key == 'price' else int(value) if key == 'id' else value for key, value in row.items()}
+                    for row in csv_reader
+                    ]
+    read_function = rcf if source == "csv" else rjf
+
+    try:
+        products = read_function(file)
+    except FileNotFoundError:
+        return render_template('product_display.html', error=f"File {file} notfound")
+
+    if product_id:
+        product_id = int(product_id)
+        products = [i for i in products if i['id'] == product_id]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+
+    return render_template('product_display.html', products=products)
+
+if __name__ == "__main__":
+    app.run(debug=True)
